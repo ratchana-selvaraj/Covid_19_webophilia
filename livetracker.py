e7accbd5bba673b74f6c9079536d9b008011eaa6
Rach <59865047+ratchana-selvaraj@users.noreply.github.com>
2020-05-16 19:58:06 +0530

    Update livetracker.py

--- livetracker.py
+++ livetracker.py
@@ -1,16 +1,24 @@
-def corona_details():
-    import pandas as pd
-    cor_df=pd.read_csv('https://raw.githubusercontent.com/h2oai/covid19-datasets/master/data/India/CaseDetailsIndia.csv')
-    cor_df.head()
-    corona_count=cor_df.groupby('State/UnionTerritory').sum()[['Cured','Deaths','Confirmed']]
-    return corona_count
-corona_count = corona_details()
-
+def Corona_State():
+	import pandas as pd
+	coronadf=pd.read_csv('https://api.covid19india.org/csv/latest/state_wise.csv')
+	coronadf.head()
+	corona_count=coronadf.groupby('State').sum()[['Delta_Confirmed','Delta_Deaths','Delta_Recovered']]
+	corona_count.head()
+	return corona_count
+def corona_dist():
+	corona2=pd.read_csv('https://api.covid19india.org/csv/latest/district_wise.csv')
+	corona2.head()
+	mdu=corona2[corona2['District'].str.contains('Madurai')]
+	mdu_c=mdu.groupby('District').sum()[['Confirmed','Recovered','Active']]
+	mdu_c.head()
+	return mdu_c
+mdu_c=corona_dist()
+corona_count = Corona_Sate()
 from flask import Flask,render_template,request
 import sys
 app=Flask(__name__)
 @app.route('/')
 def home():
-    return render_template('corona.html',table= corona_count)
+    return render_template('corona.html',table= mdu_c)
 if __name__=="__main__":
 	app.run(debug='True')
