9f111d3bc9ded331518139cb545f6ad548cf0212
Rach <59865047+ratchana-selvaraj@users.noreply.github.com>
2020-05-17 19:38:09 +0530

    Update livetracker.py

--- livetracker.py
+++ livetracker.py
@@ -1,5 +1,5 @@
+import pandas as pd
 def Corona_State():
-	import pandas as pd
 	coronadf=pd.read_csv('https://api.covid19india.org/csv/latest/state_wise.csv')
 	coronadf.head()
 	corona_count=coronadf.groupby('State').sum()[['Delta_Confirmed','Delta_Deaths','Delta_Recovered']]
@@ -13,12 +13,14 @@ def corona_dist():
 	mdu_c.head()
 	return mdu_c
 mdu_c=corona_dist()
+pair1=[(District,Confirmed,Recovered,Active) for District,Confirmed,Recovered,Active in zip(mdu_c.index,mdu_c['Confirmed'],mdu_c['Recovered'],mdu_c['Active'])]
 corona_count = Corona_State()
+pair2=[(State,Delta_Confirmed,Delta_Deaths,Delta_Recovered) for State,Delta_Confirmed,Delta_Deaths,Delta_Recovered in zip(corona_count.index,corona_count['Delta_Confirmed'],corona_count['Delta_Deaths'],corona_count['Delta_Recovered'])]
 from flask import Flask,render_template,request
 import sys
 app=Flask(__name__)
 @app.route('/')
 def home():
-    return render_template('corona.html',table= mdu_c)
+    return render_template('corona.html',table = mdu_c,map=corona_count,pair1=pair1,pair2=pair2)
 if __name__=="__main__":
 	app.run(debug='True')
