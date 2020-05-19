3ddf111ac3e0b463311fcf56cef4f8ad50b291c2
ManjariAlagar <56513108+ManjariAlagar@users.noreply.github.com>
2020-05-19 21:46:05 +0530

    newsfeed

---
+++ news.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template
+from newsapi import NewsApiClient
+
+
+
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def Index():
+    newsapi = NewsApiClient(api_key="b0f75ce660c0466a9a98c2478f8abb62")
+    topheadlines = newsapi.get_top_headlines(sources="the-times-of-india")
+
+
+    articles = topheadlines['articles']
+
+    desc = []
+    news = []
+    img = []
+
+
+    for i in range(len(articles)):
+        myarticles = articles[i]
+
+
+        news.append(myarticles['title'])
+        desc.append(myarticles['description'])
+        img.append(myarticles['urlToImage'])
+
+
+
+    mylist = zip(news, desc, img)
+
+
+    return render_template('index.html', context = mylist)
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
