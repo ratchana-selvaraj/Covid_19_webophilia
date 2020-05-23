79f38ba8adc2ffe153f5286b1cc8d5f910043a54
SubikshaGB <48596109+Mishri10@users.noreply.github.com>
2020-05-23 11:50:15 +0530

    Code v3

--- completecode.py
+++ completecode.py
@@ -1,82 +1,89 @@
-#final code
-#imports
-from flask import *
-from flask import Flask,render_template,request,redirect,url_for
-from flask_mysqldb import MySQL
-import MySQLdb
+from flask import Flask, render_template,request
 import pandas as pd
-import sys
+import plotly.express as pg
+import folium
+import urllib
 import GetOldTweets3 as got3
-import langid
-from translation import google,bing,ConnectError
-from googletrans import Translator
+import pickle
+import tensorflow
 import nltk
 from nltk.stem import WordNetLemmatizer
 import numpy
 from tensorflow.python.framework import ops
-import tensorflow
 import random
 import json
 import tflearn
-app=Flask(__name__)
-db=MySQLdb.connect("localhost","root","Goodluck","pm")
-#-------CHATBOT SECTION-------#
-#class to create dictionary that stores the dates and tweets
+import langid
+import MySQLdb
+from newsapi import NewsApiClient
+app = Flask(__name__)
+from translation import google,bing,ConnectError
+from googletrans import Translator
 class my_dictionary(dict): 
+
     # __init__ function 
     def __init__(self): 
-        self = dict()       
+        self = dict() 
+
     # Function to add key:value 
     def add(self, key, value): 
         self[key] = value 
+translator = Translator()
 lemmatizer = WordNetLemmatizer()
 ignore_words=['?','.','!']
 with open('intents.json') as file:
     data = json.load(file)
-words = []
-labels = []
-docs_x = []
-docs_y = []
-for intent in data['intents']:
-	for pattern in intent['patterns']:
-		wrds = nltk.word_tokenize(pattern)
-		words.extend(wrds)
-		docs_x.append(wrds)
-		docs_y.append(intent["tag"])
-		if intent['tag'] not in labels:
-			labels.append(intent['tag'])
-words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_words]
-words = sorted(list(set(words)))
-labels = sorted(labels)
-training = []
-output = []
-out_empty = [0 for _ in range(len(labels))]
-for x, doc in enumerate(docs_x):
-	bag = []
-	wrds = [lemmatizer.lemmatize(w.lower()) for w in doc]
-	for w in words:
-		if w in wrds:
-			bag.append(1)
-		else:
-			bag.append(0)
-	output_row = out_empty[:]
-	output_row[labels.index(docs_y[x])] = 1
-	training.append(bag)
-	output.append(output_row)
-training = numpy.array(training)
-output = numpy.array(output)
+try:
+    with open("data.pickle", "rb") as f:
+        words, labels, training, output = pickle.load(f)
+except:
+	words = []
+	labels = []
+	docs_x = []
+	docs_y = []
+	for intent in data['intents']:
+		for pattern in intent['patterns']:
+			wrds = nltk.word_tokenize(pattern)
+			words.extend(wrds)
+			docs_x.append(wrds)
+			docs_y.append(intent["tag"])
+			if intent['tag'] not in labels:
+				labels.append(intent['tag'])
+	words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_words]
+	words = sorted(list(set(words)))
+	labels = sorted(labels)
+	training = []
+	output = []
+	out_empty = [0 for _ in range(len(labels))]
+	for x, doc in enumerate(docs_x):
+		bag = []
+		wrds = [lemmatizer.lemmatize(w.lower()) for w in doc]
+		for w in words:
+			if w in wrds:
+				bag.append(1)
+			else:
+				bag.append(0)
+		output_row = out_empty[:]
+		output_row[labels.index(docs_y[x])] = 1
+		training.append(bag)
+		output.append(output_row)
+	with open("data.pickle", "wb") as f:
+		pickle.dump((words, labels, training, output), f)
+tensorflow.reset_default_graph()
+
+net = tflearn.input_data(shape=[None, len(training[0])])
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
+net = tflearn.regression(net)
+model = tflearn.DNN(net)
 try:
     model.load("model.tflearn")
 except:
-	ops.reset_default_graph()
-	net = tflearn.input_data(shape=[None, len(training[0])])
-	net = tflearn.fully_connected(net, 8)
-	net = tflearn.fully_connected(net, 8)
-	net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
-	net = tflearn.regression(net)
-	model = tflearn.DNN(net)
-	model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
-	model.save("model.tflearn")
+    model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
+    model.save("model.tflearn")
+training = numpy.array(training)
+output = numpy.array(output)
 def bag_of_words(s, words):
     bag = [0 for _ in range(len(words))]
 
@@ -103,10 +110,117 @@ def get_bot_response():
 			return(str(random.choice(responses))) 
 			quit()
 		else:
-			return(str(random.choice(responses)))
-#-------TWEETS SCRAPING SECTION-------#			
-translator = Translator()
-@app.route('/',methods=['POST','GET'])
+			return(str(random.choice(responses))) 
+@app.route('/')
+def Index():
+	newsapi = NewsApiClient(api_key="b0f75ce660c0466a9a98c2478f8abb62")
+	topheadlines = newsapi.get_top_headlines(sources="the-times-of-india")
+	articles = topheadlines['articles']
+	desc = []
+	news = []
+	img = []
+	for i in range(len(articles)):
+		myarticles = articles[i]
+		news.append(myarticles['title'])
+		desc.append(myarticles['description'])
+		img.append(myarticles['urlToImage'])
+	mylist = zip(news, desc, img)
+	mdu_c=corona_dist()
+	corona_count = Corona_State()
+	fig=graph_1().to_html()
+	html_map=m._repr_html_()
+	return render_template('index.html', context = mylist,table = mdu_c,map=corona_count,pair1=pair1,pair2=pair2,fig=fig,cmap=html_map)
+	
+def Corona_State():
+    coronadf=pd.read_csv('https://api.covid19india.org/csv/latest/state_wise.csv')
+    coronadf.head()
+    corona_count=coronadf.groupby('State').sum()[['Delta_Confirmed','Delta_Deaths','Delta_Recovered']]
+    corona_count.head()
+    pair2=[(State,Delta_Confirmed,Delta_Deaths,Delta_Recovered) for State,Delta_Confirmed,Delta_Deaths,Delta_Recovered in zip(corona_count.index,corona_count['Delta_Confirmed'],corona_count['Delta_Deaths'],corona_count['Delta_Recovered'])]
+    return corona_count
+def corona_dist():
+    corona2=pd.read_csv('https://api.covid19india.org/csv/latest/district_wise.csv')
+    corona2.head()
+    mdu=corona2[corona2['District'].str.contains('Madurai')]
+    mdu_c=mdu.groupby('District').sum()[['Confirmed','Recovered','Active']]
+    mdu_c.head()
+    return mdu_c
+def graph_1():
+    df=pd.read_csv('https://raw.githubusercontent.com/datasets/covid-19/master/data/time-series-19-covid-combined.csv')
+    df
+    cdf=df[df['Country/Region'].str.contains('India')]
+    cols=['Date','Confirmed']
+    cdf1=cdf[cols]
+    cdf1
+    fig=pg.bar(cdf1,x='Date',y='Confirmed',title = 'Corona confirmation all across India : Time series')
+    return fig
+df=pd.read_csv('https://raw.githubusercontent.com/datasets/covid-19/master/data/time-series-19-covid-combined.csv')
+df= df[(df.Date.isin(['2020-05-19']))]
+m=folium.Map(location=[21.0	,78.0],tiles='Stamen toner',zoom_start= 4 )
+folium.Circle(location= [21.0	,78.0	],radius=10000, color='gold', fill=True,popup='{}Confirmed'.format(50000)).add_to(m)
+def circle_maker(x):
+    folium.Circle(location= [x[0],x[1]],
+                  radius=float(x[2]*10),
+                  color='gold',
+                  fill=True,
+                  popup='{}\nConfirmed cases: {}'.format(x[3], x[2])).add_to(m)
+df[['Lat','Long','Confirmed','Country/Region']].apply(lambda x: circle_maker(x),axis =1)
+html_map=m._repr_html_()
+fig=graph_1()
+mdu_c=corona_dist()
+pair1=[(District,Confirmed,Recovered,Active) for District,Confirmed,Recovered,Active in zip(mdu_c.index,mdu_c['Confirmed'],mdu_c['Recovered'],mdu_c['Active'])]
+corona_count = Corona_State()
+pair2=[(State,Delta_Confirmed,Delta_Deaths,Delta_Recovered) for State,Delta_Confirmed,Delta_Deaths,Delta_Recovered in zip(corona_count.index,corona_count['Delta_Confirmed'],corona_count['Delta_Deaths'],corona_count['Delta_Recovered'])]
+
+@app.route("/public")
+def public():
+    return render_template("font-awesome.html")
+@app.route("/donation") 
+def donation():
+	return render_template("basic_elements.html")
+@app.route("/donate",methods=["GET","POST"])
+def donate():
+    if request.method=="POST":
+        pd=request.form
+        Amount=pd['amt']
+        payment=pd['pay']
+        fname=pd['fname']
+        lname=pd['lname']
+        Mail=pd['email']
+        phone=pd['phn']
+        db=MySQLdb.connect("localhost","root","Alohomora123","Donate")
+        cur=db.cursor()
+        cur.execute("INSERT INTO Donate(Firstname,Lastname,Email,Phone,Amount,Method) VALUES(%s,%s,%s,%s,%s,%s)",(fname,lname,Mail,phone,Amount,payment))
+        db.commit()
+        cur.close()
+        return render_template('checkout', phone=phone)
+@app.route("/checkout/<phone>")
+def checkout(phone):
+    db=MySQLdb.connect("localhost","root","Goodluck","pm")
+    cur=db.cursor()
+    query="select * from Donate where Phone="+phone
+    cur.execute(query)
+    m=cur.fetchone()
+    cur.close()
+    return render_template("checkout.html" ,m=m)
+@app.route("/Thankyou",methods=["GET","POST"])
+def thankyou():
+    return render_template("volenteer.html")
+@app.route("/volenteer",methods=["GET","POST"])
+def volenteer():
+	if (request.method=='POST'):
+		vfname=vfnames.append(request.form['Vfname'])
+		vlname=vlnames.append(request.form['Vlname'])
+		vemail=vemails.append(request.form['email'])
+		vphone=vphones.append(request.form['Phn'])
+		vadd=vaddrs.append(request.form['addr'])
+	return render_template("volenteer.html")
+#@app.route('/volunteerdisplay',methods=['POST','GET'])
+#def volunteerdisplay():	
+@app.route("/helpdesk")
+def helpdesk():
+    return render_template("helpdesk.html")
+@app.route('/tweets',methods=['POST','GET'])
 def tweet_scrap():
 	username1="Vijayabaskarofl"
 	count=20
@@ -164,82 +278,7 @@ def tweet_scrap():
 				dict_obj_eng3.add(str(tweets3.date),translated.text)
 		except:
 			continue
-	return render_template('tweets.html',dict_obj_eng1=dict_obj_eng1,dict_obj_eng2=dict_obj_eng2,dict_obj_eng3=dict_obj_eng3,dict_obj_tam1=dict_obj_tam1,dict_obj_tam2=dict_obj_tam2,dict_obj_tam3=dict_obj_tam3)
-#-------DONATION FORM SECTION-------#
-@app.route("/index")
-def index():
-	return render_template("form.html")
-	return render_template("contact.html")
-@app.route("/donate",methods=["GET","POST"])
-def donate():
-    if request.method=="POST":
-        pd=request.form
-        Amount=pd['amt']
-        payment=pd['pay']
-        fname=pd['fname']
-        lname=pd['lname']
-        Mail=pd['email']
-        phone=pd['phn']
-        db=MySQLdb.connect("localhost","root","Goodluck","pm")
-        cur=db.cursor()
-        cur.execute("INSERT INTO Donation1(Firstname,Lastname,Email,Phone,Amount,Method) VALUES(%s,%s,%s,%s,%s,%s)",(fname,lname,Mail,phone,Amount,payment))
-        cur1=db.cursor()
-        cur.execute("INSERT INTO Donate(Firstname,Lastname,Email,Phone,Amount,Method) VALUES(%s,%s,%s,%s,%s,%s)",(fname,lname,Mail,phone,Amount,payment))
-        db.commit()
-        cur.close()
-        return render_template("checkout.html")
-        return redirect(url_for('checkout', phone=phone))
-@app.route("/checkout/<phone>")
-def checkout(phone):
-    db=MySQLdb.connect("localhost","root","Goodluck","pm")
-    cur=db.cursor()
-    query="select * from Donate where Phone="+phone
-    cur.execute(query)
-    m=cur.fetchone()
-    cur.close()
-    return render_template("checkout.html" ,m=m)
-@app.route("/Thankyou",methods=["GET","POST"])
-def thankyou():
-    return render_template("Thankyou.html")
-#-------LIVE TRACKER SECTION-------#
-def Corona_State():
-	coronadf=pd.read_csv('https://api.covid19india.org/csv/latest/state_wise.csv')
-	coronadf.head()
-	corona_count=coronadf.groupby('State').sum()[['Delta_Confirmed','Delta_Deaths','Delta_Recovered']]
-	corona_count.head()
-	return corona_count
-def corona_dist():
-	corona2=pd.read_csv('https://api.covid19india.org/csv/latest/district_wise.csv')
-	corona2.head()
-	mdu=corona2[corona2['District'].str.contains('Madurai')]
-	mdu_c=mdu.groupby('District').sum()[['Confirmed','Recovered','Active']]
-	mdu_c.head()
-	return mdu_c
-def graph_1():
-    df=pd.read_csv('https://raw.githubusercontent.com/datasets/covid-19/master/data/time-series-19-covid-combined.csv')
-    df
-    cdf=df[df['Country/Region'].str.contains('India')]
-    cols=['Date','Confirmed']
-    cdf1=cdf[cols]
-    cdf1
-    fig=pg.bar(cdf1,x='Date',y='Confirmed',title = 'Corona confirmation all across India : Time series')
-    return fig
-fig=graph_1()
-mdu_c=corona_dist()
-pair1=[(District,Confirmed,Recovered,Active) for District,Confirmed,Recovered,Active in zip(mdu_c.index,mdu_c['Confirmed'],mdu_c['Recovered'],mdu_c['Active'])]
-corona_count = Corona_State()
-pair2=[(State,Delta_Confirmed,Delta_Deaths,Delta_Recovered) for State,Delta_Confirmed,Delta_Deaths,Delta_Recovered in zip(corona_count.index,corona_count['Delta_Confirmed'],corona_count['Delta_Deaths'],corona_count['Delta_Recovered'])]
-@app.route('/')
-def live_tracker():
-	mdu_c=corona_dist()
-	corona_count = Corona_State()
-	fig=graph_1().to_html()
-	return render_template('index.html',table = mdu_c,map=corona_count,pair1=pair1,pair2=pair2,fig=fig)
-#News flash
-#def flashnews():
-#search button function
-@app.route('/')
-def home():
-    return render_template('index.html')
-if __name__ == '__main__':
-    app.run(debug=True)
+	return render_template('tweet.html',dict_obj_eng1=dict_obj_eng1,dict_obj_eng2=dict_obj_eng2,dict_obj_eng3=dict_obj_eng3,dict_obj_tam1=dict_obj_tam1,dict_obj_tam2=dict_obj_tam2,dict_obj_tam3=dict_obj_tam3)
+if __name__ == "__main__":
+    app.run(debug=True) 
+	
